ae824e3b834f5818907b1b3a70e32f07baf45813
Emilly <emilly.r.nascimento6@aluno.senai.br>
2026-02-12 15:50:55 -0300

    início da criação do componente card

---
+++ components/card.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Card = () => {
+  return (
+    <View style={styles.card}>
+      <Text>Início do card</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: #e85c5c,
+  },
+});
