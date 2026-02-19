dba1319cb5388993b3d64652b8bee5bcdb11fd88
Emilly <emilly.r.nascimento6@aluno.senai.br>
2026-02-19 08:08:38 -0300

    comando para tirar a barra de navegação padrão do aplicativo.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,13 +2,11 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
+    // Tirar a barra de navegação padrão
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" options={{ title: "Home" }} />
-
       <Stack.Screen name="detalhes" options={{ title: "Detalhes" }} />
-
       <Stack.Screen name="sobre" options={{ title: "Sobre" }} />
-
       <Stack.Screen name="cardteste" options={{ title: "CardTeste" }} />
     </Stack>
   );
